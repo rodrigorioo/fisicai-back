0087809f6a3af426d8595eb9ea2fe12e3970aca9
Rodrigo <rodrigo.rio@outook.com.ar>
2023-09-18 19:31:59 -0300

    + CORS + BodyParser + Morgan

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 import express, { Express } from 'express';
 import routes from "./app/routes";
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 require('dotenv').config();
 
 // Setup
 const app: Express = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
 
 // Routes
 app.use('/', routes);
